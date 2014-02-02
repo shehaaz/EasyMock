8946855b6b9a0950050a9609f31f12073f3e3385
shehaaz <shehaaz@gmail.com>
2014-02-01 21:29:07 -0800

    completed TEST class

--- test/com/vogella/testing/easymock/first/IncomeCalculatorTest.java
+++ test/com/vogella/testing/easymock/first/IncomeCalculatorTest.java
@@ -25,40 +25,64 @@ public class IncomeCalculatorTest {
 
 	@Test
 	public void testCalc1() {
+		
+		//Mocked Interface is given as parameter
+		calc.setCalcMethod(calcMethod);
+		
 		// Setting up the expected value of the method call calc
 		expect(calcMethod.calc(Position.BOSS)).andReturn(70000.0).times(2);
 		expect(calcMethod.calc(Position.PROGRAMMER)).andReturn(50000.0);
 		// Setup is finished need to activate the mock
-		replay(calcMethod);
+		replay(calcMethod); //Need to reply() after expect()
 
-		calc.setCalcMethod(calcMethod);
 		try {
 			calc.calc();
 			fail("Exception did not occur");
 		} catch (RuntimeException e) {
 
 		}
+		
 		calc.setPosition(Position.BOSS);
 		assertEquals(70000.0, calc.calc(), 0);
 		assertEquals(70000.0, calc.calc(), 0);
+		
 		calc.setPosition(Position.PROGRAMMER);
 		assertEquals(50000.0, calc.calc(), 0);
+		
 		calc.setPosition(Position.SURFER);
+		
 		verify(calcMethod);
 	}
 
-	@Test(expected = RuntimeException.class)
+	@Test (expected = RuntimeException.class)
 	public void testNoCalc() {
 		calc.setPosition(Position.SURFER);
 		calc.calc();
+		
+//		expect(calcMethod.calc(Position.SURFER)).andReturn(10000.0);
+//		replay(calcMethod); //Need to reply after expect
+//		
+//		calc.setCalcMethod(calcMethod);
+//		calc.setPosition(Position.SURFER);
+//		
+//		assertEquals(10000.0, calc.calc(), 0);	
 	}
 
-	@Test(expected = RuntimeException.class)
+	//RuntimeException because ImcomeCalculator Position not set.
+	@Test (expected = RuntimeException.class)
 	public void testNoPosition() {
 		expect(calcMethod.calc(Position.BOSS)).andReturn(70000.0);
 		replay(calcMethod);
 		calc.setCalcMethod(calcMethod);
-		calc.calc();
+		assertEquals(70000.0, calc.calc(), 0);
+		verify(calcMethod);
+		
+//		expect(calcMethod.calc(Position.BOSS)).andReturn(70000.0);
+//		replay(calcMethod);
+//		calc.setCalcMethod(calcMethod);
+//		calc.setPosition(Position.BOSS);
+//		assertEquals(70000.0, calc.calc(), 0);
+//		verify(calcMethod);
 	}
 
 	@Test(expected = RuntimeException.class)
